5dc7374c49ad195f6d2b6f8465607e924f167c6f
otknoy <otknoy@gmail.com>
2014-03-03 14:52:57 +0900

    add tf_idf method and fix ngram method

--- nlp.py
+++ nlp.py
@@ -65,12 +65,32 @@ def document_frequency(texts):
         df[term] = [term in text for text in texts].count(True)
     return df
 
+def inverse_document_frequency(texts):
+    df = document_frequency(texts)
+    idf = {}
+    for t in df.keys():
+        idf[t] = 1.0/df[t]
+    return idf
+
+def tf_idf(texts):
+    tfidf_list = []
+    idf = inverse_document_frequency(texts)
+    for terms in texts:
+        tf = term_frequency(terms)
+        tfidf = {}
+        for t in idf.keys():
+            if not tf.has_key(t):
+                tfidf[t] = 0.0
+            else:
+                tfidf[t] = tf[t] * idf[t]
+        tfidf_list.append(tfidf)
+    return tfidf_list
+    
+
 def ngram(terms, n=2):
     ret = []
-    i = 0
-    while i+n < len(terms):
+    for i in range(0, len(terms)-n+1):
         ret.append(terms[i:i+n])
-        i += 1
     return ret
 
 
@@ -106,3 +126,18 @@ if __name__ == '__main__':
     df = document_frequency(texts)
     for t, w in df.items():
         print t, w
+    print
+
+    print 'inverse document frequency'
+    idf = inverse_document_frequency(texts)
+    for t, w in idf.items():
+        print t, w
+    print
+
+    print 'tf-idf'
+    tfidf_list = tf_idf(texts)
+    for tfidf in tfidf_list:
+        for t, w in tfidf.items():
+            print w,
+        print
+
